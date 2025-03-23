a437b29063f3ebc6878ad7347803be6d876ddb11
Vijay Mani Dev Kaluri <vijayjonathan143@gmail.com>
2025-03-23 03:20:47 -0500

    prod server fix

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Set nginx environment variable from React environment variable
+export BACKEND_URL=$REACT_APP_BACKEND_URL
+
 # Replace environment variables in nginx config
-envsubst '$REACT_APP_BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
